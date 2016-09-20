f32ce9865ff427d372f7100be9b9be06eb35943b
jeff christian <jchri@MacBook-Pro.local>
2016-09-20 14:10:16 -0700

    seems to be complete

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -16,8 +16,26 @@
 * rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
 *
 */
-
-var rockPaperScissors = function (
-) {
+debugger
+var rockPaperScissors = function (limit) {
   // TODO: your solution here
-};
+  var possibilities = ['rock','paper','scissors']
+
+  var arr = [];
+    function shoot(limit, result){
+      if(limit === 0){
+        arr.push(result)
+        return ''
+      }
+      for(var i = 0; i < possibilities.length; i++){
+        shoot((limit - 1), result.concat(possibilities[i]))
+      }
+    }
+        
+
+
+  shoot(limit,[])
+  return arr;
+}
+
+console.log(rockPaperScissors(3))
