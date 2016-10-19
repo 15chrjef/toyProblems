58cf2ba433e052af808b5bb0c84749ab7913dd56
jeff christian <jchri@MacBook-Pro-2.loca>
2016-10-19 09:31:31 -0700

    finish multiply, modulu, divide

--- implementingOperators/implementingOperators.js
+++ implementingOperators/implementingOperators.js
@@ -12,14 +12,41 @@
 
 var multiply = function(x, y) {
   // TODO: should return the product of x * y
+  //x groups of y
+  var total = 0;
+  var count = 0;
+  while (count < x) {
+    count++;
+    total += y;
+  }
+  console.log(total);
+  return total;
 };
 
 var divide = function(x, y) {
   // TODO: should return the quotient of x / y
+  //x goes into y, z number of times
+  var total = 0;
+  var numer = x;
+  while ( (numer - y) >= 0 ) {
+    total ++;
+    numer -= y;
+  }
+  console.log(total);
+  return total;
 };
 
 var modulo = function(x, y) {
+    console.log(modulo);
+
   // TODO: should return the remainder of x / y
+  var numer = x;
+  while ( (numer - y) >= 0 ) {
+    numer -= y;
+  }
+  console.log(numer);
+  return numer;
 };
-
-
+multiply(100, 3);
+divide(30, 3);
+modulo(27, 4);
