025166339504ad4eef5df7610f7fd15620778672
Jeffrey Emerich Christian <15chrjef@gmail.com>
2017-03-28 18:37:50 -0700

    Create sum(3)(4)(5).js

---
+++ sum(3)(4)(5).js
@@ -0,0 +1,14 @@
+function sum(num1, num2){
+  if(arguments.length === 2){
+    return num1 + num2
+  }else {
+    return function(x){
+      return function(y){
+        return sum(sum(x,num1), y)
+      }
+    }
+  }
+}
+
+sum(5)(4)(3)
+
